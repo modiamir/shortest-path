9460894c21af2e939c119953e2f70f498ffc12b4
amir <amir.modarresi.13@gmail.com>
2021-02-05 03:41:56 +0330

    Add test cases (I know this is not real test case but is useful as documentation)

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"shortest-path/models"
 	"shortest-path/parsers"
+	"shortest-path/service"
 )
 
 var (
@@ -13,5 +14,48 @@ var (
 func main() {
 	Vertices = parsers.ParseAirports()
 	parsers.ParseAirportRoutes(&Vertices)
+
+	var path []models.Vertex
+
+	path = service.FindShortestPathWithMaxEdge(Vertices, *Vertices["IKA"], *Vertices["TLL"], 2)
+	if len(path) != 0 {
+		panic("Must return empty list")
+	}
+
+	path = service.FindShortestPathWithMaxEdge(Vertices, *Vertices["IKA"], *Vertices["TLL"], 3)
+	if len(path) != 3 {
+		panic("Must return list with three vertices")
+	}
+
+	path = service.FindShortestPathWithMaxEdge(Vertices, *Vertices["IKA"], *Vertices["TLL"], 4)
+	if len(path) != 3 {
+		panic("Must return list with three vertices")
+	}
+
+	path = service.FindShortestPath(Vertices, *Vertices["IKA"], *Vertices["TLL"])
+	if len(path) != 3 {
+		panic("Must return list with three vertices")
+	}
+
+	path = service.FindShortestPathWithMaxEdge(Vertices, *Vertices["IKA"], *Vertices["NDB"], 4)
+	if len(path) != 0 {
+		panic("Must return empty list")
+	}
+
+	path = service.FindShortestPathWithMaxEdge(Vertices, *Vertices["IKA"], *Vertices["NDB"], 5)
+	if len(path) != 5 {
+		panic("Must return list with five vertex")
+	}
+
+	path = service.FindShortestPathWithMaxEdge(Vertices, *Vertices["IKA"], *Vertices["NDB"], 6)
+	if len(path) != 5 {
+		panic("Must return list with five vertex")
+	}
+
+	path = service.FindShortestPath(Vertices, *Vertices["IKA"], *Vertices["NDB"])
+	if len(path) != 5 {
+		panic("Must return list with five vertex")
+	}
+
 	fmt.Println("Done")
 }
